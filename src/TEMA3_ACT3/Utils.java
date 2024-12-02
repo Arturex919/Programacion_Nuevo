0782f62c4cce3b3b915bc79262f0be25179845cd
Arturex919 <edutuacal@alu.edu.gva.es>
2024-12-02 12:26:48 +0100

    2/12

---
+++ src/TEMA3_ACT3/Utils.java
@@ -0,0 +1,17 @@
+package TEMA3_ACT3;
+
+//. Crea una clase Utils que contendrá diferentes funciones y realiza las
+//siguientes funciones, no repitas código.//
+public class Utils {
+    //2. Añade a Utils una función miles2kilometers que convierta millas a
+    //kilometros//
+    public static double miles2kilometers(double millas) {
+        double km = millas * 1.60934;
+        return km;
+    }
+    //3 ñade a Utils una función getTaxes que dada una cantidad en euros y un
+    //porcentaje devuelva el valor del impuesto a aplicar. El valor debe
+    //devolver siempre dos decimales//
+    public static double getTaxes(U)
+
+}

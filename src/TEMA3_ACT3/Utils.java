5ecb2ad36caa43d3379d2db78f2abc9a6a0495af
Arturex919 <edutuacal@alu.edu.gva.es>
2024-12-09 10:36:38 +0100

    9/12

--- src/TEMA3_ACT3/Utils.java
+++ src/TEMA3_ACT3/Utils.java
@@ -4,72 +4,82 @@ package TEMA3_ACT3;
 import TEMA3.TEMA3_ACT2.MyMath;
 
 import java.util.Scanner;
+
 public class Utils {
-    public static double miles2kilometers(double milla){
+    public static double miles2kilometers(double milla) {
         double km;
-        km= milla*1.60;
-        return km;
-    }
-    public static double kilometers2miles(double km){
-        double milla;
-        milla= km*0.62;
+        km = milla * 1.60;
         return km;
     }
 
-    public static double getTaxes(double cant,double impuesto){
-        double tax,rendodea;
+    public static double kilometers2miles(double km) {
+        double milla;
+        milla = km * 0.62;
+        return km;
+    }
+
+    public static double getTaxes(double cant, double impuesto) {
+        double tax, rendodea;
         //cant=cantidad se le multiplica por la cantidad que tenemos y dividimos para 100
-        tax=cant * (impuesto/100);
+        tax = cant * (impuesto / 100);
         //se rendondea usando math.round(tax es el resultado del paso anteior se multiplica
         //para trabajar con enteros y evitar problemas divides para 100 al formato decimal original
         // pero ya redondeado a dos decimales.
-        rendodea= Math.round(tax*100)/100;
+        rendodea = Math.round(tax * 100) / 100;
         System.out.println("El impuesto para " + cant + "€ con un " + impuesto + "% es: " + tax + "€.");
 
         return rendodea;
     }
-    public static double getNetPrice(double cant,double impuesto){
-        double calculo,total;
-        calculo=cant*(impuesto/100);
-        total=cant+calculo;
+
+    public static double getNetPrice(double cant, double impuesto) {
+        double calculo, total;
+        calculo = cant * (impuesto / 100);
+        total = cant + calculo;
         return total;
     }
-    public static String getCoins(double euros){
+
+    public static String getCoins(double euros) {
         int centavos;
         //casteas el valor para que de monedas en entero
-        centavos= (int)Math.round(euros*100);
-        int euro2=centavos/200;
-        euro2%=200;
-        int euro1=centavos/100;
-        euro1%=100;
-        int fiftycent=centavos/50;
-        centavos%=50;
-        int cent20=20;
-        cent20%=20;
-        int cent10=10;
-        cent10%=10;
-        int cent5=5;
-        cent5%=5;
-        int cent2=2;
-        cent2%=2;
-        int cent1=1;
-        cent1%=1;
+        centavos = (int) Math.round(euros * 100);
+        //el valor que introducimos anteriormente va hacer dividido en para el numero que
+        //tenemos y vamos a sacar el modular si nos da
+        int euro2 = centavos / 200;
+        centavos %= 200;
 
-        String result = ""+euro2+euro1+fiftycent+cent20+cent10+cent5;
+        int euro1 = centavos / 100;
+        centavos %= 100;
 
-        return getCoins(centavos);
+        int fiftycent = centavos / 50;
+        centavos %= 50;
+
+        int cent20 = centavos / 20;
+        centavos %= 20;
+
+        int cent10 = centavos / 10;
+        centavos %= 10;
+
+        int cent5 = centavos / 5;
+        centavos %= 5;
+
+        int cent2 = centavos / 2;
+        centavos %= 2;
+
+        int cent1 = centavos;
+
+
+        String result = "" + euro2 + euro1 + fiftycent + cent20 + cent10 + cent5 + cent2 + cent1;
+
+        return result;
 
     }
 
 
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int art,aa;
-        System.out.println("muestra numero ");
-        art= in.nextInt();
-        System.out.println("precio");
-        aa=in.nextInt();
-        getTaxes(art,aa);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduce una cantidad en euros: ");
+        double art = in.nextDouble(); // Leer la cantidad en euros
+        System.out.println("Las monedas son: " + getCoins(art));
     }
 
 }

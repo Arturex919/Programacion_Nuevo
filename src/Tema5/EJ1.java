bd3069776c7e41955361ba65d3a187f7075650c7
Arturex919 <edutuacal@alu.edu.gva.es>
2025-01-15 20:32:51 +0100

    ERROR EN EL PUSH DEL PRIMERO EJ Y SECOND

--- src/Tema5/EJ1.java
+++ src/Tema5/EJ1.java
@@ -1,6 +1,9 @@
 package Tema5;
 
+import java.util.Scanner;
+
 public class EJ1 {
+
 
 
     //Un procedimiento que imprima la matriz.//
@@ -14,32 +17,33 @@ public class EJ1 {
         }
     }
 
-    public static int maxArray (int [][] max){
-        int numMax=max[0][0];
-        for (int i=0;i<max.length;i++){
-            if (max[i][i]>numMax){
-                numMax=max[i][i];
+    //b) Una función que devuelva el máximo de la matriz.
+    public static int maxArray(int[][] max) {
+        int numMax = max[0][0];//recorrera las columnas
+        for (int i = 0; i < max.length; i++) {
+            for (int j = 0; j < max[i].length; j++) {
+                if (max[i][j] > numMax) {
+                    numMax = max[i][j];
+                }
             }
         }
-        return  numMax;
+        return numMax;
     }
-    public static int minArray(int [][] min){
-        int numMin=min[0][0];
-        for (int i=0;i<min.length;i++){
-            if (min[i][i]<numMin){
-                numMin=min[i][i];
-            }
-        }
-        return  numMin;
-    }
-    public static int mediaArray(int [][]media){
-        int numMedia=media[0][0];
-        int suma=0;
-        for (int i=0;i<media.length;i++){
 
+    //Una función que devuelva el mínimo de la matriz.
+    public static int minArray(int[][] min) {
+        int numMin = min[0][0];
+        for (int i = 0; i < min.length; i++) {
+            for (int j = 0; j < min[i].length; j++) {
+                if (min[i][j] < numMin) {
+                    numMin = min[i][j];
+                }
+            }
         }
-        return numMedia;
+        return numMin;
     }
+
+    //d) Una función que devuelva la media de la matriz.
     public static double mediaArray(double[][] media) {
         double numMedia = media[0][0];
         double suma = 0;
@@ -58,7 +62,7 @@ public class EJ1 {
     //Una función que te diga si un elemento existe en la matriz o no.
     public static boolean existeA(int[][] real, int elemetos) {
         for (int i = 0; i < real.length; i++) {
-            for (int j = 0; j < real[i].length; i++) {
+            for (int j = 0; j < real[i].length; j++) {
                 if (real[i][j] == elemetos) {
                     return true;
                 }
@@ -66,42 +70,91 @@ public class EJ1 {
         }
         return false;
     }
+
     //Una función que devuelva cuantas veces se repite un número en
     //una matriz.
-    public static int repetite(int [][]numeroR,int numero){
-        int cont=0;
-        for (int i=0;i<numeroR.length;i++){
-            for (int j=0;j<numeroR[i].length;j++){
-                if (numeroR[i][j]==numero){//si el numero en i y es igual al numero en j se repite y nos avisa
+    public static int repetite(int[][] numeroR, int numero) {
+        int cont = 0;
+        for (int i = 0; i < numeroR.length; i++) {
+            for (int j = 0; j < numeroR[i].length; j++) {
+                if (numeroR[i][j] == numero) {//si el numero en i y es igual al numero en j se repite y nos avisa
                     cont++;
                 }
             }
         }
         return cont;
     }
-    public static int sumaMatriz(int [][]suma1,int [][]suma2) {
-        int count1 = 0;//1
-        int count2 = 0;//2
-        int sum = 0;
-        int sum2 = 0;
+
+    //g) Una función que haga la suma de dos matrices.
+    public static int[][] sumaMatriz(int[][] suma1, int[][] suma2) {
         if (suma1 != suma2) {
             System.out.println("Los numeros no son iguales");
         }
+        int[][] resultado = new int[suma1.length][suma1[0].length];
         for (int i = 0; i < suma1.length; i++) {
             for (int j = 0; j < suma1[i].length; j++) {
-                sum += suma1[i][j];
-                count1++;
+                resultado[i][j] = suma1[i][j] + suma2[i][j];
             }
         }
-        return sum;
 
+        return resultado;
     }
 
+    //h) Una función que haga la resta de dos matrices.
+    public static int[][] restaMatriz(int[][] resta, int[][] resta2) {
+        if (resta != resta2) {
+            System.out.println("no son iguales");
+        }
+        int[][] resultado = new int[resta.length][resta[0].length];
+        for (int i = 0; i < resta.length; i++) {
+            for (int j = 0; j < resta[i].length; j++) {
+                resultado[i][j] = resta[i][j] - resta2[i][j];
+            }
+        }
+        return resultado;
+    }
 
-        public static void main (String[]args){
-
-
-
-
+    //) Una función que compruebe si una matriz es la matriz unidad, es
+    //decir tiene el mismo número de filas y de columnas ytiene en su
+    //diagonal principal 1s y el resto 0
+    public static boolean compruba(int[][] fila) {
+        //si no es igual a una matriz la detecta
+        if (fila.length != fila[0].length) ;
+        { //el cero porque corre las columnas
+            System.out.println("Erroooor,No es una unidad");
+        }
+        int tamañoMatriz = fila.length;//nos comprueba el taño de nuestra matriz
+        for (int i = 0; i < tamañoMatriz; i++) {
+            for (int j = 0; j < tamañoMatriz; j++) {
+                if (i == j) {
+                    if (fila[i][j] != 1) {//compruebas las diagonales para ver si es 1
+                        return false;//si es distinto a 1 nos da falso
+                    }
+                }else {
+                    if (fila[i][j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        // Declaramos una matriz de ejemplo
+        Scanner in = new Scanner(System.in);
+// Definir una matriz de ejemplo
+        int[][] matrizUnidad = {
+                {1, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1}
+        };
+        int[][] matrizNoUnidad = {
+                {1, 0, 0},
+                {0, 1, 0},
+                {0, 0, 1}
+        };
+        System.out.println(compruba(matrizUnidad));
+        System.out.println(compruba(matrizNoUnidad));
     }
 }

bd9ac1439744cceaf8c4ebe01dcc8d39eb4c920a
Arturex919 <edutuacal@edu.alu.gva.es>
2025-03-31 10:56:33 +0200

    acabar y entenderlo bien

--- src/Tema_7_Ficheros/Ejercicios/Ejercicios3/Funko.java
+++ src/Tema_7_Ficheros/Ejercicios/Ejercicios3/Funko.java
@@ -1,15 +1,16 @@
 package Tema_7_Ficheros.Ejercicios.Ejercicios3;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class Funko implements Serializable {
     private String cod;
     private String nombre;
     private String modelo;
     private double precio;
-    private String fechaLanzamiento;
+    private LocalDate fechaLanzamiento;//la cambiamos para guardar la fecha
 
-    public Funko(String cod, String nombre, String modelo, double precio, String fechaLanzamiento) {
+    public Funko(String cod, String nombre, String modelo, double precio, LocalDate fechaLanzamiento) {
         this.cod = cod;
         this.nombre = nombre;
         this.modelo = modelo;
@@ -35,12 +36,11 @@ public class Funko implements Serializable {
         return precio;
     }
 
-    public String getFechaLanzamiento() {
+    public LocalDate getFechaLanzamiento() {
         return fechaLanzamiento;
     }
 
     //setters
-
 
     public void setCod(String cod) {
         this.cod = cod;
@@ -58,10 +58,20 @@ public class Funko implements Serializable {
         this.precio = precio;
     }
 
-    public void setFechaLanzamiento(String fechaLanzamiento) {
+    public void setFechaLanzamiento(LocalDate fechaLanzamiento) {
         this.fechaLanzamiento = fechaLanzamiento;
     }
 
+    // Método para convertir el objeto a formato CSV
+    public String toCSV() {
+        return cod + "," + nombre + "," + modelo + "," + precio + "," + fechaLanzamiento;
+    }
+    // Método para crear un Funko a partir de una línea CSV
+    public static Funko fromCSV(String csvLine) {
+        String[] values = csvLine.split(",");//es para separa el csv
+        //le das los valores para que se convierta en un funko
+        return new Funko(values[0], values[1], values[2], Double.parseDouble(values[3]), LocalDate.parse(values[4]));
+    }
     @Override
     public String toString() {
         return "Funko"+"\n"+
@@ -70,5 +80,13 @@ public class Funko implements Serializable {
                 "Modelo: "+modelo+"\n"+
                 "Precio: "+precio+"\n"+
                 "Fecha de Lanzamiento: "+fechaLanzamiento;
+    }
+    //lo hacemos para que no se repita en el csv y solo nos salga una vez
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Funko funko = (Funko) obj;
+        return cod.equals(funko.cod);
     }
 }
--- src/Tema_7_Ficheros/Ejercicios/Ejercicios3/Funko_Main.java
+++ src/Tema_7_Ficheros/Ejercicios/Ejercicios3/Funko_Main.java
@@ -1,4 +1,107 @@
 package Tema_7_Ficheros.Ejercicios.Ejercicios3;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Funko_Main {
+    public static String fileName="Documentos/Ejercicios3/funkos.csv";
+    public static ArrayList<Funko> funkos = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("Ingresa una opcion");
+            showMenu();
+            opcion = in.nextInt();
+
+            switch (opcion) {
+                case 1 -> {
+                    aniadeFunko(funkos,in);
+                }
+            }
+        } while (opcion != 8);
+    }
+
+    public static void showMenu() {
+        System.out.println("1) Añadir funko");
+        System.out.println("2) Borra funko");
+        System.out.println("3) Mostrar todos los funko");
+        System.out.println("4) Mostrar funko mas costoso");
+        System.out.println("5) Mostrar la media de precios de los funkos");
+        System.out.println("6) Mostrar funko agrupados por modelos");
+        System.out.println("7) Mostrar funko del 2023");
+    }
+
+    public static void aniadeFunko(ArrayList<Funko> funkos, Scanner in) {
+        boolean sal = false;
+        int opcion;
+        while (!sal) {
+            System.out.println("1) Añade tu funko ");
+            System.out.println("2) salir");
+             opcion=in.nextInt();
+
+                 switch (opcion) {
+                     case 1 -> {
+                         in.nextLine();
+                         System.out.println("Añade el COD");
+                         String cod = in.nextLine();
+                         System.out.println("Añade el nombre");
+                         String nombre = in.nextLine();
+                         System.out.println("Añade el modelo");
+                         String modelo = in.nextLine();
+                         System.out.println("Añade el precio");
+                         double precio = in.nextDouble();
+                         in.nextLine();
+                         System.out.println("Añade la fecha de lanzamiento (YYYY-MM-DD)");
+                         LocalDate fecha = LocalDate.parse(in.nextLine());
+                         //LocalDate Guardas la fecha
+                         Funko funkoAniadido = new Funko(cod, nombre, modelo, precio, fecha);
+                         if (!funkos.contains(funkoAniadido)){
+                             funkos.add(funkoAniadido);
+                             saveFunko();
+                             String lee = "Se añadio el funko correctamente"+"\n"+
+                                     "CARACTERISCAS"+"\n"+"COD :"+cod
+                                     +"\n"+"Nombre: "+nombre+"\n"+
+                                     "Modelo: "+modelo+"\n"+"Precio "+precio+"\n"+
+                                     "Fecha: "+fecha;
+                             System.out.println(lee);
+                         }else {
+                             System.out.println("El Funko ya existe en la colección.");
+                         }
+                     }
+                     case 2 -> {
+                         System.out.println("Adiosssss");
+                         sal=true;
+                     }
+                     default -> System.out.println("Opcion no valida");
+                 }
+        }
+    }
+    public static void saveFunko(){
+        try(BufferedWriter br=new BufferedWriter(new FileWriter(fileName))){
+            for (Funko lee :funkos)
+            br.write( "Se añadio el funko correctamente"+"\n"+
+                    "CARACTERISCAS"+"\n"+"COD :"+lee.getCod()+"\n"
+                    +"\n"+"Nombre: "+lee.getNombre()+"\n"+
+                    "Modelo: "+ lee.getModelo()+"\n"+"Precio "+lee.getPrecio()+"\n"+
+                    "Fecha: "+ lee.getFechaLanzamiento());
+            br.newLine();
+    } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+        public static void eliminarFunko(Scanner in){
+        System.out.println("Nombre del funko a eliminar");
+        String remove=in.nextLine();
+        if (remove.equalsIgnoreCase(remove)){
+            funkos.remove(remove);
+        }
+    }
+
 }
